3f10132ad0c44a68a25ac6c7de68399872889fdf
kacper3615 <kacper3615@gmail.com>
2022-10-16 23:55:57 +0200

    Added lab1

---
+++ lab1/oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,39 @@
+package agh.ics.oop;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
+
+import static agh.ics.oop.Direction.*;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.print("system wystartował\n");
+        World.run(args);
+        System.out.print("system zakończył działanie");
+    }
+
+    private static Direction[] getDirection(String[] args) {
+        Direction[] directions = new Direction[args.length];
+        for (int i = 0; i < args.length; i++) {
+            directions[i] = switch (args[i]) {
+                case "f" -> FORWARD;
+                case "b" -> BACKWARD;
+                case "l" -> LEFT;
+                case "r" -> RIGHT;
+                default -> OTHER;
+            };
+        }
+        return directions;
+    }
+    private static void run(String[] args){
+        Direction[] directionsArr = getDirection(args);
+        for (Direction direction : directionsArr) {
+            switch (direction) {
+                case FORWARD -> System.out.print("Zwierzak idzie do przodu\n");
+                case BACKWARD -> System.out.print("Zwierzak idzie do tyłu\n");
+                case LEFT -> System.out.print("Zwierzak idzie w lewo\n");
+                case RIGHT -> System.out.print("Zwierzak idzie w prawo\n");
+            }
+        }
+    }
+}
