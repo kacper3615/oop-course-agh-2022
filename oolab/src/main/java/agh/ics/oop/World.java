99322328f97be73bc063614dd1005bc42c28d05c
Kacper <kacper3615@gmail.com>
2022-10-25 02:09:00 +0200

    Added lab2

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -6,7 +6,13 @@ public class World {
     public static void main(String[] args) {
         System.out.print("Start\n");
         World.run(args);
-        System.out.print("Stop");
+        System.out.print("Stop\n");
+
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
 
     private static Direction[] getDirection(String[] args) {
---
+++ oolab/src/test/java/agh/ics/oop/Vector2dTest.java
@@ -0,0 +1,11 @@
+package agh.ics.oop;
+
+import org.junit.jupiter.api.Test;
+
+public class Vector2dTest {
+
+    @Test
+    public void nextTest(){
+        
+    }
+}
